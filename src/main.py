71488e6b3de3ad3fd13d394357f5246be2310938
kento2247 <tkento1985@icloud.com>
2026-01-12 00:56:11 +0900

    update

--- src/main.py
+++ src/main.py
@@ -338,4 +338,6 @@ if __name__ == "__main__":
     if args.mode == "train":
         ...
     elif args.mode == "test":
-        evaluator.test()
+        test_metrics = evaluator.test()
+        lb_text = format_leaderboard_text(evaluator.meta_data, test_metrics)
+        print(f"Leaderboard Text:\n{lb_text}")
