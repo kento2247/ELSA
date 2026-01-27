2693bffda95635da050b7a0c4c30f5433c3394e7
kento2247 <tkento1985@icloud.com>
2026-01-27 16:17:50 +0900

    🐛 inference_from_dataメソッドのneg_prompt引数をリスト型に変更し、空のリストを渡すように修正。preprocess.pyでのneg_promptの処理を改善。

--- src/preprocess.py
+++ src/preprocess.py
@@ -18,8 +18,7 @@ from pydantic import BaseModel, Field
 from sam_audio import SAMAudio, SAMAudioProcessor
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from transformers import (AutoModelForCausalLM, AutoTokenizer, ClapModel,
-                          ClapProcessor)
+from transformers import AutoModelForCausalLM, AutoTokenizer, ClapModel, ClapProcessor
 
 from CLAPSep.model.CLAPSep import CLAPSep
 from dataset import TTADataset
@@ -869,7 +868,7 @@ class CLAPSepSeparator(AudioSeparator):
                 separated = self.model.inference_from_data(
                     audio_tensor,
                     pos_prompt=[prompt],
-                    neg_prompt=[""],
+                    neg_prompt=[],
                 )
                 separated_audios.append(separated[0].cpu())
         return separated_audios
@@ -1340,7 +1339,9 @@ def create_diff_audio(dataloader, feats_dir: str):
             # Use gather to get the actual waveform values with maximum absolute value at each time step
             merged_separated = torch.gather(
                 separated_audios, 0, max_abs_idx.unsqueeze(0)
-            ).squeeze(0)  # (C, T)
+            ).squeeze(
+                0
+            )  # (C, T)
 
             diff_audio = original_audio - merged_separated
             diff_audio = torch.clamp(diff_audio, -1.0, 1.0)
